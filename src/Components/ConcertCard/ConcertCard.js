3a0ebc82802939cbbc077433c5a68b39b7796461
Nikolaj <mail@nikolajstrands.dk>
2022-10-16 08:42:42 +0200

    Fix errors

--- src/Components/ConcertCard/ConcertCard.js
+++ src/Components/ConcertCard/ConcertCard.js
@@ -3,23 +3,32 @@ import { Image } from "react-bootstrap";
 
 export default function ConcertCard({ concert }) {
 
-  const timeFormatterOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  };
+  function getTime() {
+    if (concert.time.getHours() === 1) {
+      return concert.time.toLocaleDateString("da-DK", {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+    }
+    return concert.time.toLocaleDateString("da-DK", {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+  }
 
   return (
-    <a style={{pointerEvents: concert.link === "" ? "none" : "inherit"}} href={concert.link} target="_blank" rel="noreferrer">
+    <a style={{ pointerEvents: concert.link === "" ? "none" : "inherit" }} href={concert.link} target="_blank" rel="noreferrer">
       <div className={styles.wrapper}>
         <div className={styles.imageWrapper}>
-          <Image fluid src={process.env.PUBLIC_URL + "/assets/" + concert.imageFileName} />
+          <Image fluid src={process.env.PUBLIC_URL + "/assets/venues/" + concert.imageFileName} />
         </div>
         <div className={styles.textWrapper}>
           <p className={styles.concertTitle}>{concert.title}</p>
-          <p className={styles.details}>{concert.time.toLocaleDateString("da-DK", timeFormatterOptions)}</p>
+          <p className={styles.details}>{getTime()}</p>
           <p className={styles.details}>{concert.place}</p>
           <p className={styles.details}>{concert.admission}</p>
         </div>
