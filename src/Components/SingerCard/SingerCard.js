86c93a55fd8beebd4f8db4bf3f7179a00be962d5
Nikolaj <mail@nikolajstrands.dk>
2024-03-17 22:11:11 +0100

    Adjust frontend

--- src/Components/SingerCard/SingerCard.js
+++ src/Components/SingerCard/SingerCard.js
@@ -4,8 +4,8 @@ import { Image } from "react-bootstrap";
 export default function SingerCard({ singer }) {
     return (
         <div className={styles.wrapper}>
-            <div>
-                <Image fluid src={singer.portraitFileName} />
+            <div className={styles.imageWrapper}>
+                <Image fluid src={"uploads/portraits/" + singer.portraitFileName} />
             </div>
             <div className={styles.textWrapper}>
                 <p className={styles.singerName}>{singer.name}</p>
